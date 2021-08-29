f4d90d33cf5f0cf1341c7c3b657b6d8fa1359c74
Aulia Faturrachman <afatur.code@gmail.com>
2021-08-29 09:30:28 +0700

    fifth commit

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.static(path.join(__dirname, "public")));
+const cors = require("cors");
+app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const db = require("./config/key").mongoURI;
+const router = require("./router");
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.use(express.static(path.join(__dirname, "build")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));

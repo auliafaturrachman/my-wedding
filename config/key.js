3521e6d2cf6320fddca6537bf51f7366e7942381
auliafaturrachman <afatur.code@gmail.com>
2023-05-30 12:28:56 +0700

    Reactjs - fix gallery mobile

--- config/key.js
+++ config/key.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://usr-01:sqh128qo35@cluster0.44w8c.mongodb.net/wedding?retryWrites=true&w=majority",
+	mongoURI:
+		"mongodb+srv://faturbdd:eGy145sLDoSwLBoi@cluster0.vxtbxpb.mongodb.net/db_wedding?retryWrites=true&w=majority",
 };
+// mongodb+srv://usr-01:sqh128qo35@cluster0.44w8c.mongodb.net/wedding?retryWrites=true&w=majority

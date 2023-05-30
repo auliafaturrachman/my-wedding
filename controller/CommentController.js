0f3161aee791ddb72e706062a898de92ef37372e
auliafaturrachman <afatur.code@gmail.com>
2023-05-30 13:02:41 +0700

    Nodejs - alter table comment add visibility

--- controller/CommentController.js
+++ controller/CommentController.js
@@ -1,34 +1,35 @@
 const Comment = require("../model/Comment");
 
 const index = async () => {
-  try {
-    return await Comment.find().exec();
-  } catch (ex) {
-    return ex;
-  }
+	try {
+		return await Comment.find({ visibility: "true" }).exec();
+	} catch (ex) {
+		return ex;
+	}
 };
 
 const create = async (req) => {
-  try {
-    let newComment = new Comment({
-      name: req.body.name,
-      attendance: req.body.attendance,
-      wishes: req.body.wishes,
-    });
+	try {
+		let newComment = new Comment({
+			name: req.body.name,
+			attendance: req.body.attendance,
+			wishes: req.body.wishes,
+			visibility: "true",
+		});
 
-    await newComment.save();
-    return newComment;
-  } catch (ex) {
-    return ex;
-  }
+		await newComment.save();
+		return newComment;
+	} catch (ex) {
+		return ex;
+	}
 };
 
 const show = async ({ _id }) => {
-  return await Comment.findById({ _id }).exec();
+	return await Comment.findById({ _id }).exec();
 };
 
 module.exports = {
-  index,
-  create,
-  show,
+	index,
+	create,
+	show,
 };
--- model/Comment.js
+++ model/Comment.js
@@ -2,22 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  attendance: {
-    type: String,
-    required: true,
-  },
-  wishes: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+	name: {
+		type: String,
+		required: true,
+	},
+	attendance: {
+		type: String,
+		required: true,
+	},
+	wishes: {
+		type: String,
+		required: true,
+	},
+	visibility: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
 module.exports = Comment = mongoose.model("comment", CommentSchema);
